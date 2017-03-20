491256a8b3fe956e0158b6b9fd9040df64873597
alexisbrabo <alexis_brabo@hotmail.com>
2017-03-20 14:44:16 -0300

    Mais exercícios adicionados.

--- lista_de _exercicios_1/2.py
+++ lista_de _exercicios_1/2.py
@@ -1,17 +1,17 @@
-#Escreva um programa que leia um valor em metros e o exiba convertido em milímetros
+# Escreva um programa que leia um valor em metros e o exiba convertido em milímetros
 
 import sys
 
 a = input('Escreva um valor em metros para ser convertido para milímetros\n')
 
 try:
-  metros = int(a)
+    metros = int(a)
 except ValueError:
-  try:
-    metros = float(a)
-  except ValueError:
-    print('Precisa ser um número')
-    sys.exit(0)
+    try:
+        metros = float(a)
+    except ValueError:
+        print('Precisa ser um número')
+        sys.exit(0)
 
 milimetros = float(metros * 1000)
 
