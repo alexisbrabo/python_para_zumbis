ec2433f6d3a1e5b78c2ae18a19de6065bc254a64
alexisbrabo <alexis_brabo@hotmail.com>
2018-06-07 15:36:48 -0300

    Mais Exercicios

--- diversos/textToSpeech.py
+++ diversos/textToSpeech.py
@@ -1,20 +1,23 @@
-import win32com.client as wincl
+import pyttsx3
 from tkinter import *
 
 
 def text2Speech():
     text = e.get()
-    speak = wincl.Dispatch("SAPI.SpVoice")
-    speak.Speak(text)
+    engine = pyttsx3.init()
+    engine.say(text)
+    engine.setProperty('rate', 200)  # 120 words per minute
+    engine.setProperty('volume', 0.9)
+    engine.runAndWait()
 
 
 # window configs
 tts = Tk()
 tts.wm_title("Text to Speech")
-tts.geometry("225x105")
+tts.geometry("290x110")
 tts.config(background="#708090")
 
-f = Frame(tts, height=280, width=500, bg="#bebebe")
+f = Frame(tts, height=280, width=550, bg="#bebebe")
 f.grid(row=0, column=0, padx=10, pady=5)
 lbl = Label(f, text="Enter your Text here : ")
 lbl.grid(row=1, column=0, padx=10, pady=2)
