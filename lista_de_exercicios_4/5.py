6e5a560477a04cbf4ded7dc4db2c3b7953226ef2
alexisbrabo <alexis_brabo@hotmail.com>
2018-06-11 15:01:44 -0300

    Mais exercicios

---
+++ lista_de_exercicios_4/5.py
@@ -0,0 +1,18 @@
+"""
+Seja o mesmo texto acima “splitado”. 
+Calcule quantas palavras possuem uma das letras “python” e que tenham mais de 4 caracteres. 
+Não se esqueça de transformar maiúsculas para minúsculas e de remover antes os caracteres especiais.
+"""
+
+texto = "The Python Software Foundation and the global Python community welcome and encourage participation by everyone. Our community is based on mutual respect, tolerance, and encouragement, and we are working to help each other live up to these principles. We want our community to be more diverse: whoever you are, and whatever your background, we welcome you."
+texto = texto.replace(",", "")
+texto = texto.replace(".", "")
+listaTexto = texto.split()
+lista_python = []
+
+for p in listaTexto:
+    a = p.lower()
+    if a[0] in "python" and len(a) > 4:
+        lista_python.append(p)
+
+print(lista_python)
